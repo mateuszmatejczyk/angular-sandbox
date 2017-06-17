9108070ce8f252f24cfdb02acd89cefaab449c4a
Mateusz Matejczyk <mateusz.matejczyk@gmail.com>
2017-06-17 17:33:39 -0400

    Router for the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 
@@ -10,6 +12,7 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
