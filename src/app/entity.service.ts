21c7e7fcba4b226e45290acd25663dcb948def2a
Mateusz Matejczyk <mateusz.matejczyk@gmail.com>
2017-06-26 09:45:35 -0400

    Small refactor pdates in service.

--- src/app/entity.service.ts
+++ src/app/entity.service.ts
@@ -13,25 +13,30 @@ export class EntityService {
   readonly entities: Observable<Entity[]>;
 
   constructor() {
+    this.dataSubject = new BehaviorSubject(this.extractEntities());
+    this.entities = this.dataSubject.asObservable();
+
     this.addEntity({ id: 0, name: 'Swinia', points: 0 });
     this.addEntity({ id: 1, name: 'Seba', points: 0 });
     this.addEntity({ id: 2, name: 'Maciore', points: 0 });
     this.addEntity({ id: 3, name: 'Pulpe', points: 0 });
-
-    this.dataSubject = new BehaviorSubject(this.extractEntities());
-    this.entities = this.dataSubject.asObservable();
 
     setInterval( () => {
       const i = Math.floor(Math.random() * this.data.size);
       const e = this.data.get(i);
       e.points += 10;
 
-      this.dataSubject.next(this.extractEntities());
+      this.updateSubscribers();
     }, 200);
   }
 
   addEntity(e: Entity): void {
     this.data.set(e.id, e);
+    this.updateSubscribers();
+  }
+
+  private updateSubscribers(): void {
+    this.dataSubject.next(this.extractEntities());
   }
 
   private extractEntities(): Entity[] {
